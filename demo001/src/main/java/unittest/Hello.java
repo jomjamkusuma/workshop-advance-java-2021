706fc4d77e9f4c8027c56a99cd2e7778702207c8
Kusuma Jongpattana <kusuma.jo@kbtg.tech>
2021-08-02 14:09:38 +0700

    Isolate/Independent test state

--- demo001/src/main/java/unittest/Hello.java
+++ demo001/src/main/java/unittest/Hello.java
@@ -4,4 +4,17 @@ public class Hello {
     public String hi(String name) {
         return "Hello, " + name;
     }
+
+    UserDB userDB;
+    public String workWithDb(int id) {
+        return userDB.getNameByID(id);
+    }
+
+}
+
+class UserDB{
+
+    public String getNameByID(int id) {
+        return null;
+    }
 }
--- demo001/src/test/java/unittest/HelloTest.java
+++ demo001/src/test/java/unittest/HelloTest.java
@@ -6,14 +6,29 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class HelloTest {
+
     @Test
     @DisplayName("this is first case writing")
     public void case01(){
         Hello hello = new Hello();
-
         String actualResult = hello.hi("somkiat");
-
         assertEquals("Hello, somkiat", actualResult);
     }
 
+    @Test
+    @DisplayName("ทดสอบกับ database (I=Isolate/Independent)")
+    public void case02(){
+        Hello hello = new Hello();
+        hello.userDB = new UserDBWithSuccess();
+        String name = hello.workWithDb(1);
+        assertEquals("somkiat", name);
+    }
+
+    private class UserDBWithSuccess extends UserDB {
+
+        @Override
+        public String getNameByID(int id) {
+            return "somkiat";
+        }
+    }
 }
