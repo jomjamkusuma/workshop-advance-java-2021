0c5b34eab77f568b65979e2595537819c2a04464
Kusuma Jongpattana <kusuma.jo@kbtg.tech>
2021-08-03 14:25:06 +0700

    add Spy of random number

--- demo001/src/main/java/testdouble/GenerateIdService.java
+++ demo001/src/main/java/testdouble/GenerateIdService.java
@@ -3,16 +3,16 @@ package testdouble;
 import java.util.Random;
 
 public class GenerateIdService {
-
+    // Dependency
     private Random random = new Random();
 
-    public void setRandom(Random random){
+    public void setRandom(Random random) {
         this.random = random;
     }
 
-    public String get() {
-        Random random = new Random(); // Dependency
+    public String get(Object name) {
         int number = random.nextInt(10);
+//        random.nextInt(10);
         String id = "XYZ" + number;
         return id;
     }
--- demo001/src/test/java/testdouble/GenerateIdServiceTest.java
+++ demo001/src/test/java/testdouble/GenerateIdServiceTest.java
@@ -17,23 +17,49 @@ public class GenerateIdServiceTest {
      */
 
     @Test
-    @DisplayName("ID = XYZ7")
+    @DisplayName("Demo Stub Random :: ID = XYZ7")
     public void case01() {
         GenerateIdService service = new GenerateIdService();
         service.setRandom(new StubRandom(7));
-        String id = service.get();
+        String id = service.get(null);
         assertEquals("XYZ7", id);
     }
 
-    private class StubRandom extends Random {
-        private final int number;
-        public StubRandom(int number) {
-            this.number = number;
-        }
+    @Test
+    @DisplayName("Demo spy random :: Called nextInt()")
+    public void case02() {
+        GenerateIdService service = new GenerateIdService();
+        SpyRandom spyRandom = new SpyRandom();
+        service.setRandom(spyRandom);
+        service.get(null);
+        spyRandom.verify(1);
+    }
 
-        @Override
-        public int nextInt(int bound) {
-            return number;
+}
+
+class SpyRandom extends Random {
+    private int counter = 0;
+    @Override
+    public int nextInt(int bound) {
+        this.counter++;
+        return 100000;
+    }
+
+    public void verify(int expectedCount) {
+        if(expectedCount != counter) {
+            fail("จำนวนการเรียกใช้งาน method nextInt() ไม่ถูกต้อง");
         }
+    }
+}
+
+class StubRandom extends Random {
+    private final int number;
+    public StubRandom(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public int nextInt(int bound) {
+        return number;
     }
 }
